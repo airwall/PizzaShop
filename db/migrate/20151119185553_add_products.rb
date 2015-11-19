1d3085580d6bd0758a5df6e9f3039fbe8c5f3883
Belinschi Dima <belinski.d@gmail.com>
2015-11-19 21:13:01 +0200

    add products to database

---
+++ db/migrate/20151119185553_add_products.rb
@@ -0,0 +1,31 @@
+class AddProducts < ActiveRecord::Migration
+  def change
+
+  	Product.create  :title => 'Hawaiian', 
+  					:description => 'This is Hawaiian pizza', 
+  					:price => 72,
+  					:size => 30,
+  					:is_spicy => false,
+  					:is_veg => false,
+  					:is_best_offer => false,
+  					:path_to_image => '/img/1.png'
+
+  	Product.create  :title => 'Pepperoni', 
+  					:description => 'Nice pepperoni pizza', 
+  					:price => 78,
+  					:size => 30,
+  					:is_spicy => false,
+  					:is_veg => false,
+  					:is_best_offer => false,
+  					:path_to_image => '/img/2.png'
+
+  	Product.create  :title => 'Vegetarian', 
+  					:description => 'Amazing vegeterian pizza', 
+  					:price => 75,
+  					:size => 30,
+  					:is_spicy => false,
+  					:is_veg => true,
+  					:is_best_offer => false,
+  					:path_to_image => '/img/3.png'	  					
+  end
+end
