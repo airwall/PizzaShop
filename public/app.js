d456b44e8248d3d20fe301fc9a1ebf9af9685ccd
Belinschi Dima <belinski.d@gmail.com>
2015-11-23 20:25:46 +0200

    keep total number of products ordered

--- public/app.js
+++ public/app.js
@@ -15,7 +15,23 @@ function something()
 
 function add_to_cart(id)
 {
- 	var x = window.localStorage.getItem('product_' + id);
+
+	var keyid = 'product_' + id;
+ 	var x = window.localStorage.getItem(keyid);
  	x = x * 1 + 1;
- 	window.localStorage.setItem('product_' + id, x);
+ 	window.localStorage.setItem(keyid, x);
+
+ 	for(var i in localStorage)
+		{
+		    console.log(localStorage[i]);
+		}
+		
+		var total = 0; //Total numbers of products ordered
+		for(var i=0, len=localStorage.length; i<len; i++) {
+		    var key = localStorage.key(i);
+		    var value = localStorage[key];
+		    total = total  + value*1;
+		    console.log("Total = " + total);
+		}
 }
+
