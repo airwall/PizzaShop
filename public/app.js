be2ea52afd8144bcae9bb933790fdaeb57142466
Belinschi Dima <belinski.d@gmail.com>
2015-11-23 22:12:01 +0200

    Show numbers of products on cart

--- public/app.js
+++ public/app.js
@@ -16,22 +16,29 @@ function something()
 function add_to_cart(id)
 {
 
-	var keyid = 'product_' + id;
- 	var x = window.localStorage.getItem(keyid);
+	var key_id = 'product_' + id;
+ 	var x = window.localStorage.getItem(key_id);
  	x = x * 1 + 1;
- 	window.localStorage.setItem(keyid, x);
+ 	window.localStorage.setItem(key_id, x);
+
+ 	var total = 0; //Total numbers of products ordered
+
+	
 
  	for(var i in localStorage)
 		{
 		    console.log(localStorage[i]);
 		}
-		
-		var total = 0; //Total numbers of products ordered
-		for(var i=0, len=localStorage.length; i<len; i++) {
+			for(var i=0, len=localStorage.length; i<len; i++) {
 		    var key = localStorage.key(i);
 		    var value = localStorage[key];
 		    total = total  + value*1;
 		    console.log("Total = " + total);
+		    window.localStorage.setItem("total", total);
+		    
 		}
+
+
 }
+
 
