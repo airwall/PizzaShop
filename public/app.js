79b97488d91f194721d1d2c9a0c612e2b18307ec
Belinschi Dima <belinski.d@gmail.com>
2015-11-23 22:40:26 +0200

    Fix 2 Show numbers of products on cart

--- public/app.js
+++ public/app.js
@@ -21,23 +21,6 @@ function add_to_cart(id)
  	x = x * 1 + 1;
  	window.localStorage.setItem(key_id, x);
 
- 	// var total = 0; //Total numbers of products ordered
-
-	
-
- 	// for(var i in localStorage)
-		// {
-		//     console.log(localStorage[i]);
-		// }
-		// 	for(var i=0, len=localStorage.length; i<len; i++) {
-		//     var key = localStorage.key(i);
-		//     var value = localStorage[key];
-		//     total = total  + value*1;
-		//     console.log("Total = " + total);
-		//     window.localStorage.setItem("total", total);
-		    
-		// }
-
 
 }
 
