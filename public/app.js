a3c5791cde676c593e6e0becfec8eb5168abfc54
Belinschi Dima <belinski.d@gmail.com>
2015-11-24 16:42:15 +0200

    add to function numbers_of_products_in_cart()

--- public/app.js
+++ public/app.js
@@ -1,27 +1,29 @@
 
-function something()
-{
-
-	var x =window.localStorage.getItem('cc');
-	
-	x = x * 1 + 1;
-
-	window.localStorage.setItem('cc', x);
-
-	alert(x);	
-	
-}
-
-
 function add_to_cart(id)
 {
-
 	var key_id = 'product_' + id;
  	var x = window.localStorage.getItem(key_id);
  	x = x * 1 + 1;
  	window.localStorage.setItem(key_id, x);
-
-
 }
 
+
+//Calculate total number of products & display this in class 'output'
+function number_of_products_in_cart()
+{
+	var total = 0;	
+
+for(var i=0, len=localStorage.length; i<len; i++) 
+{
+	var key = localStorage.key(i);
+	var value = localStorage[key];
+
+	if(key.indexOf('product_') == 0)
+	{
+		total = total  + value*1;   
+	}
+}
+	$('.output').html('('+ total +')');
+	return total;
+}
 
